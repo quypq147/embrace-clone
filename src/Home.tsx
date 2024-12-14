a524987110dc1f0623d272205585f1e9f10345ce
quypq147 <phanngocquy1472004@gmail.com>
2024-12-14 22:41:24 +0700

    update

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,13 +4,13 @@ import { Button } from "./components/ui/button";
 
 export default function Home() {
   return (
-    <section className="grid m-1 sm:flex sm:m-10">
-        <div className="hero-title grid justify-center items-center">
-            <div className="frame-104">
+    <section className="grid m-1 justify-center items-center sm:flex sm:m-10">
+        <div className="hero-title grid">
+            <div className="frame-104 grid gap-3">
                 <h1 className="text-8xl flex-wrap frame-104_h1 mx-10">We Take Care Of Your Brand</h1>
-                <p className="frame-104_p">We care about our work and we care about our clients</p>
+                <p className="frame-104_p mx-10">We care about our work and we care about our clients</p>
             </div>
-            <div className="frame-55 flex gap-5">
+            <div className="frame-55 mx-10 flex gap-5">
                 <input className="rounded-3xl border p-3 h-8 " placeholder="Enter your email"></input>
                 <Button className="frame-55_btn rounded-3xl">Let's Talk</Button>
             </div>
--- src/HowItsWork.tsx
+++ src/HowItsWork.tsx
@@ -1,4 +1,8 @@
 import Img from "./assets/frame_111.png";
+import Discovery from "./assets/HowItsWorkAssets/discovery.svg";
+import Execution from "./assets/HowItsWorkAssets/execution.svg";
+import Measurement from "./assets/HowItsWorkAssets/Measurement.svg";
+import Strategy from "./assets/HowItsWorkAssets/strategy.svg";
 
 const HowitsWork = () => {
   return (
@@ -12,15 +16,49 @@ const HowitsWork = () => {
             goals and challenges.
           </p>
         </div>
-        <img className="" src={Img}/>
+        <img className="w-80 h-50" src={Img} />
       </div>
       <div className="group-2 grid gap-5">
-        <div className="frame_106">
-
+        <div className="frame_106 flex gap-5">
+          <img src={Discovery} />
+          <div className="chld-group">
+            <h1 className="text-3xl">Discovery</h1>
+            <p className="text-sm">
+              We meet with you to learn about your business, your goals, and
+              your target audience.
+            </p>
+          </div>
         </div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className="frame_107 flex gap-5">
+          <img src={Strategy} />
+          <div className="chld-group">
+            <h1 className="text-3xl">Strategy</h1>
+            <p className="text-sm">
+              We develop a customized marketing strategy that is based on your
+              unique needs and goals.
+            </p>
+          </div>
+        </div>
+        <div className="frame_108 flex gap-5">
+          <img src={Execution} />
+          <div className="chld-group">
+            <h1 className="text-3xl">Execution</h1>
+            <p>
+              We execute our strategy using the latest digital marketing tools
+              and techniques.
+            </p>
+          </div>
+        </div>
+        <div className="frame_109 flex gap-5">
+          <img src={Measurement} />
+          <div className="chld-group">
+            <h1 className="text-3xl">Measurement</h1>
+            <p>
+              We track the results of our campaigns so that we can make
+              adjustments as needed.
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
